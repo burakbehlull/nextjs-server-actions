4095b7a7ce0fda9708ebd47c431139977804458b
burakbehlull <burakyabgu@gmail.com>
2023-12-14 04:18:22 +0300

    feat: Create Todos page

---
+++ src/app/todos/page.tsx
@@ -0,0 +1,30 @@
+"use client"
+import axios from "axios"
+import { useState, useEffect } from "react"
+import HeaderText from "../utils/HeaderText"
+export default function page(){
+    const [todos, setTodos] = useState([])
+    useEffect(()=> {
+        async function getTodos(){
+            try {
+                const Todos = await axios.get('/api')
+                setTodos(Todos.data['todos'])   
+            } catch (err) {
+                console.log('hata: ', err)
+            }
+
+        }
+        getTodos()
+    }, [])
+    console.log(todos)
+    return(
+        <>
+            <HeaderText>Todos</HeaderText>
+            {todos?.map((item, key)=><div key={key} className="border-2 border-gray-400 max-w-[40vw] m-6">
+                <h2>{item?.title}</h2>
+                <p> {item?.description} </p>
+            </div>
+            )}
+        </>
+    )
+}
